a0df4dd9b3ccf5087a82c3b4e8daeadf788094a1
Richard Cairncross <m.abdullah9122002@gmail.com>
2025-01-10 05:42:17 -0500

    about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,10 +1,10 @@
 export default function AboutMe() {
   return (
     <div className="max-w-2xl mx-auto px-4">
-      <h2 className="text-4xl font-bold mb-4">Looking for 2025 Summer Internship.</h2>
+      <h2 className="text-4xl font-bold mb-4">Looking for 2025 Summer Internship</h2>
       <p className="text-lg">
         My name is Muhammad Abdullah, my friends call me Abdullah.
-        I like building products for people and mechanical systems that control stuff.
+        I like building products for people and mechanical systems that control things.
         I work really hard and learn even faster. You can reach out to me on{' '}
         <a 
           href="https://x.com/notabd7" 
@@ -14,11 +14,10 @@ export default function AboutMe() {
         >
           𝕏.com
         </a>.
-        I am the most active there so just DM me. Below are some products I worked hard on but even more 
-        importantly, I care alot about. 
-        I have not written my tech stack and other things here because
-        I do a lot of things. My projects will give you an idea of what
-        I am good at but give me anything that you want to build, I will build it.
+        I am most active there. Below are some products I worked hard on and deeply care about. 
+        I have not listed my tech stack and other details here because
+        I work on a lot of things. My projects will give you an idea of what
+        I am good at, but if you give me anything you want to build, I will build it. Fast.
       </p>
     </div>
   )
